05fbdc44a5e6dcca34c9a27a8a1d9cf52262994e
mauriciodesign <mauricio.a.design@gmail.com>
2020-02-17 23:39:24 -0300

    Se corrige medida en javascript y keyframe

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $(window).on('scroll', function () {
-    if ($(window).scrollTop() > 280) {
+    if ($(window).scrollTop() > 190) {
       $('.features__graphic_svg').addClass('features__graphic_animated');
     } else {
       $('.features__graphic_svg').removeClass('features__graphic_animated');
